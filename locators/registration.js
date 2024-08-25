e3247807a28cc9ec15c86e24537a303dd210b7bf
Dezzly <dezzly21@gmail.com>
2024-08-25 21:58:42 +0300

    Removed examples and added registration.spec.js

---
+++ locators/registration.js
@@ -0,0 +1,19 @@
+const locators =
+{
+    signUp: ".hero-descriptor_btn",
+    registrationModalTitle: "//h4[text()='Registration']",     
+    nameLabel: "//label[text()='Name']",
+    lastNameLabel: "//label[text()='Last name']",
+    emailLabel: "//label[text()='Email']",
+    passwordLabel: "//label[text()='Password']",
+    reenterPasswordLabel: "//label[text()='Re-enter password']",
+    registrationButton: "//button[text()='Register']",
+    name: "#signupName",      
+    lastName: "#signupLastName",    
+    email: "#signupEmail",    
+    password: "#signupPassword",    
+    repeatPassword: "#signupRepeatPassword",
+    allertForField: "div.invalid-feedback p"
+}
+
+export default locators;
